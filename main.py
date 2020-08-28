3cf05de2d0980e09c9217b74bd6ae3cf46d0f98e
KieranMurdocca <kkm5754@psu.edu>
2020-08-28 22:43:33 +0000

    fahrenheit to celsius

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 celsius = float(input("Enter temperature in celsius: "))
-fahrenheit = float((celsius * 1.8) + 32)
-print("%0.1f° in Celsius is equivalent to %0.1f° Fahrenheit." %(celsius, fahrenheit))
+fahrenheit = float((celsius * 1.80) + 32)
+print("%0.1f° in Celsius is equivalent to %0.2f° Fahrenheit." %(celsius, fahrenheit))
 #link I referenced for help #https://beginnersbook.com/2019/05/python-program-to-convert-celsius-to-fahrenheit-and-vice-versa/
